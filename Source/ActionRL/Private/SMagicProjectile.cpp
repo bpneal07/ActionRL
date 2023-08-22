58fc0e79ddeb60b0a9c7e4363696ab871128903e
bpneal07 <bpneal07@gmail.com>
2023-08-22 15:18:34 -0400

    Updated HUD

--- Source/ActionRL/Private/SMagicProjectile.cpp
+++ Source/ActionRL/Private/SMagicProjectile.cpp
@@ -32,15 +32,15 @@ ASMagicProjectile::ASMagicProjectile()
 
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if(OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
-		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(OtherActor->GetComponentByClass(USAttributeComponent::StaticClass()));
-		if (AttributeComp)
-		{
-			AttributeComp->ApplyHealthChange(-20.0f);
+			USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(OtherActor->GetComponentByClass(USAttributeComponent::StaticClass()));
+			if (AttributeComp)
+			{
+				AttributeComp->ApplyHealthChange(-20.0f);
 
-			Destroy();
-		}
+				Destroy();
+			}
 	}
 }
 
